3b1e7055264ed9e270296fc3bcf2082be5a9c0f6
ZLFR <120394243+mmalek11@users.noreply.github.com>
2024-04-28 22:46:22 +0300

    Add files via upload

---
+++ cfx-gabz-pacificbank/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GABZ'
+description 'PACIFICBANK'
+version '2.0.0'
+
+this_is_a_map 'yes'
---
+++ sxb/__resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ -- By A&S STORE.
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+	
+    'audioconfig/mbnzc63eng_game.dat151.rel',
+    'audioconfig/mbnzc63eng_sounds.dat54.rel',
+    'sfx/dlc_mbnzc63eng/mbnzc63eng.awc',
+    'sfx/dlc_mbnzc63eng/mbnzc63eng_npc.awc'
+}
+-- By A&S STORE.
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mbnzc63eng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mbnzc63eng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mbnzc63eng'
+-- By A&S STORE.
